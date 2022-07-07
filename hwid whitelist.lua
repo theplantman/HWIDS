56ce05a13d52bd2ea4e458a6b800d3f4a98f573c
amogusimpostor <99112532+theplantman@users.noreply.github.com>
2022-07-07 13:28:24 +0800

    Update hwid whitelist.lua

--- hwid whitelist.lua
+++ hwid whitelist.lua
@@ -21,6 +21,7 @@ local hwidtable1 = {
 local hwidtable2 = {
     "07fa932704e684c3fbb6b638ab09d3d5bc9f6ae9ac631baf7e00596fed416d8613a8b455d35825774802b5a848272d419f892088f5eca78f253f6c5644a51f3a"; --Wing#8610--
     "D8903A045BF44D107EB050FCA1BA2C401004"; --Error.#7073
+    "d071eeb5ddeb44dd032bec0cf15bdc81b1200a53d1dca62e6391d8b54ab8c061eaaf148cd4a405bc5ff5e7402c766a31218405bff5373b0b13495baba829c70d"; --swag man#5009
 }
 return {
     table1check = function()
